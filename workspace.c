866423d9d3225e86ae91d72ce2e7fd35102fd5de
cultbaus <cultbaus@gmail.com>
2023-10-28 17:33:41 -0700

    Prepare for multi-workspaces

--- workspace.c
+++ workspace.c
@@ -19,10 +19,43 @@ static void free_space(space_t *space)
 	free_windows(&space->subsidiaries);
 }
 
-static void arrange(workspace_t *ws)
+static void unmap_s(space_t *s)
 {
-	space_t *s = &ws->workspaces[ws->current];
+	if (s->primary != XCB_NONE) {
+		xcb_unmap_window(conn, s->primary);
+	}
+	for (size_t i = 0; i < s->subsidiaries.size; i++) {
+		xcb_unmap_window(conn, s->subsidiaries.list[i]);
+	}
+}
 
+static void map_s(space_t *s)
+{
+	if (s->primary != XCB_NONE) {
+		xcb_map_window(conn, s->primary);
+	}
+	for (size_t i = 0; i < s->subsidiaries.size; i++) {
+		xcb_map_window(conn, s->subsidiaries.list[i]);
+	}
+}
+
+space_t *current_s(workspace_t *ws)
+{
+	return &ws->spaces[ws->current];
+}
+
+void switch_workspace(workspace_t *ws, size_t i)
+{
+	space_t *prev = current_s(ws);
+	unmap_s(prev);
+
+	ws->current = i;
+	space_t *next = current_s(ws);
+	map_s(next);
+}
+
+static void arrange(space_t *s)
+{
 	uint32_t c_gap = gap_size - (2 * border_size);
 
 	uint32_t f_width = screen->width_in_pixels;
@@ -36,7 +69,7 @@ static void arrange(workspace_t *ws)
 		uint32_t p_width = h_width - (c_gap * 3) / 2;
 		uint32_t s_x_offset = (c_gap * 2) + p_width;
 
-		window_move_resize(s->primary, c_gap, c_gap, p_width, gf_height);
+		window_resize(s->primary, p_width, gf_height);
 
 		uint32_t s_height = (gf_height - (s->subsidiaries.size - 1) * gap_size) / s->subsidiaries.size;
 		uint32_t y = c_gap;
@@ -56,20 +89,19 @@ void setup_ws(workspace_t *ws)
 {
 	ws->current = 0;
 	for (size_t i = 0; i < MAX_WORKSPACES; i++) {
-		setup_space(&ws->workspaces[i]);
+		setup_space(&ws->spaces[i]);
 	}
 }
 
 void free_ws(workspace_t *wss)
 {
 	for (size_t i = 0; i < MAX_WORKSPACES; i++) {
-		free_space(&wss->workspaces[i]);
+		free_space(&wss->spaces[i]);
 	}
 }
 
-void add_to_ws(workspace_t *ws, xcb_window_t window)
+void add_to_ws(space_t *s, xcb_window_t window)
 {
-	space_t *s = &ws->workspaces[ws->current];
 	if (s->primary == XCB_NONE) {
 		s->primary = window;
 		return;
@@ -79,9 +111,8 @@ void add_to_ws(workspace_t *ws, xcb_window_t window)
 	}
 }
 
-void remove_from_ws(workspace_t *ws, xcb_window_t window)
+void remove_from_ws(space_t *s, xcb_window_t window)
 {
-	space_t *s = &ws->workspaces[ws->current];
 	if (s->primary == window) {
 		if (s->subsidiaries.size > 0) {
 			s->primary = s->subsidiaries.list[0];
@@ -94,21 +125,21 @@ void remove_from_ws(workspace_t *ws, xcb_window_t window)
 	remove_from_windows(&s->subsidiaries, window);
 }
 
-void map_ws(workspace_t *ws, xcb_window_t win)
+void map_ws(space_t *s, xcb_window_t win)
 {
 	window_border_width(win, border_size);
 	window_border_color(win, border_color);
 	window_sloppy_focus(win);
 	xcb_flush(conn);
 
-	add_to_ws(ws, win);
-	arrange(ws);
+	add_to_ws(s, win);
+	arrange(s);
 	xcb_map_window(conn, win);
 }
 
-void unmap_ws(workspace_t *ws, xcb_window_t win)
+void unmap_ws(space_t *s, xcb_window_t win)
 {
-	remove_from_ws(ws, win);
-	arrange(ws);
+	remove_from_ws(s, win);
+	arrange(s);
 	xcb_unmap_window(conn, win);
 }
--- workspace.h
+++ workspace.h
@@ -13,17 +13,20 @@ typedef struct {
 } space_t;
 
 typedef struct {
-	space_t workspaces[MAX_WORKSPACES];
+	space_t spaces[MAX_WORKSPACES];
 	size_t current;
 } workspace_t;
 
 void setup_ws(workspace_t *);
 void free_ws(workspace_t *);
-void add_to_ws(workspace_t *, xcb_window_t);
-void remove_from_ws(workspace_t *, xcb_window_t);
+void add_to_ws(space_t *, xcb_window_t);
+void remove_from_ws(space_t *, xcb_window_t);
 
-void map_ws(workspace_t *, xcb_window_t);
-void unmap_ws(workspace_t *, xcb_window_t);
+void map_ws(space_t *, xcb_window_t);
+void unmap_ws(space_t *, xcb_window_t);
+
+space_t *current_s(workspace_t *);
+void switch_ws(workspace_t *);
 
 extern workspace_t workspace;
 extern xcb_screen_t *screen;
