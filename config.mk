964adc7759cc90ea8ffcbcea4202d922b62c1781
cultbaus <cultbaus@proton.me>
2023-11-02 23:12:38 -0700

    Remove debug flag

--- config.mk
+++ config.mk
@@ -5,5 +5,5 @@ PREFIX  = /usr
 CC      = cc
 LD      = $(CC)
 
-CFLAGS  = -g -std=c99 -pedantic -Wall -Os -I/usr/include
+CFLAGS  = -std=c99 -pedantic -Wall -Os -I/usr/include
 LDFLAGS = -lxcb -lxcb-util -lxcb-keysyms -L/usr/lib
